2c5a43486207769cd29b49664c3d7ddbc534d0f6
lofta <tranquilcode@gmail.com>
2021-04-26 06:37:03 +0300

    Initial commit

---
+++ src/main/java/com/packagename/myapp/Application.java
@@ -0,0 +1,38 @@
+package com.packagename.myapp;
+
+import com.vaadin.flow.component.page.AppShellConfigurator;
+import com.vaadin.flow.component.page.Push;
+import com.vaadin.flow.server.PWA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.UnicastProcessor;
+
+/**
+ * The entry point of the Spring Boot application.
+ *
+ * Use the @PWA annotation make the application installable on phones, tablets
+ * and some desktop browsers.
+ *
+ */
+@SpringBootApplication
+@PWA(name = "Project Base for Vaadin with Spring", shortName = "Project Base")
+@Push
+public class Application extends SpringBootServletInitializer implements AppShellConfigurator {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    UnicastProcessor<ChatMessage> publisher(){
+        return UnicastProcessor.create();
+    }
+    @Bean
+    Flux<ChatMessage> messages(UnicastProcessor<ChatMessage> publisher) {
+        return publisher.replay(30).autoConnect();
+    }
+}
---
+++ src/main/java/com/packagename/myapp/ChatMessage.java
@@ -0,0 +1,19 @@
+package com.packagename.myapp;
+
+public class ChatMessage {
+    private String from;
+    private String message;
+
+    public ChatMessage(String from, String message) {
+        this.from = from;
+        this.message = message;
+    }
+
+    public String getFrom() {
+        return from;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
